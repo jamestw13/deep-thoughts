4bb02f301174d28e670114c86129aa923cba40bc
TJ James <jamestw13@gmail.com>
2022-01-18 13:21:23 -0600

    setup graphql users and thoughts

--- server/server.js
+++ server/server.js
@@ -1,10 +1,38 @@
+// import express
 const express = require('express');
+
+// import mongoose database connection
 const db = require('./config/connection');
+
+// import graphQL apollo
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware,
+  });
+
+  // start the Apollo server
+  await server.start();
+
+  // integrate Apollo server with Express app as middleware
+  server.applyMiddleware({app});
+
+  // log where we can use GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// initialize Apollo server
+startServer();
+
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 db.once('open', () => {
