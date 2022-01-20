946ccb31032bbc5ab5654db65968ba3823a76b91
TJ James <jamestw13@gmail.com>
2022-01-20 14:25:42 -0600

    set up concurrently package

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,19 +4,13 @@ const secret = 'mysecretshhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function ({username, email, _id}) {
-    const payload = {username, email, _id};
-
-    return jwt.sign({data: payload}, secret, {expiresIn: expiration});
-  },
-
   authMiddleware: function ({req}) {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
-      token = token.split('').pop().trim;
+      token = token.split(' ').pop().trim;
     }
 
     // if no token, return request object as is
@@ -34,5 +28,10 @@ module.exports = {
 
     // return updated request object
     return req;
+  },
+  signToken: function ({username, email, _id}) {
+    const payload = {username, email, _id};
+
+    return jwt.sign({data: payload}, secret, {expiresIn: expiration});
   },
 };
