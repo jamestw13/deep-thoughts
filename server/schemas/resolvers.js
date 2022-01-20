1a827f9791302b29239661b6548178bf13ceeaaf
TJ James <jamestw13@gmail.com>
2022-01-20 12:20:50 -0600

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,9 +4,12 @@ const {signToken} = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async (parent, arg, context) => {
+    me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({}).select('-__v -password').populate('thoughts').populate('friends');
+        const userData = await User.findOne({_id: context.user._id})
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
         return userData;
       }
       throw new AuthenticationError('Not logged in');
